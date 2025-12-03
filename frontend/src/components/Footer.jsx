a22231d3093f42667747c219fd231503157e83ce
Emily Ho <emilyy.ho@mail.utoronto.ca>
2025-12-03 14:37:42 -0500

    Revert "Testing deployment"
    This reverts commit 6eda33ad2fa50f79a6c057cc7e7583b8806df279.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import React from 'react'
+
 const Footer = () => {
   return (
     <div className='items-center text-white bg-dark_blue p-4 flex flex-row-reverse'>
-      <p className='text-sm mr-24'>Emily Ho © 2025</p>
+      <p className='text-sm mr-24'>Emily Ho ©2025</p>
       <p className='text-sm pr-4 mr-4 border-r-2'>Study Helper</p>
     </div>
   )
